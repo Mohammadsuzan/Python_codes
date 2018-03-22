094da814902e5bee6d9c1bb857cba77eb3b9290a
Mohammadsuzan.Shaikh@LENDINGKART.LOCAL <Mohammadsuzan.Shaikh@LENDINGKART.LOCAL>
2018-03-22 18:08:28 +0530

    Second commit

--- data_summary/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Feb 26 14:34:18 2018
-
-@author: Mohammadsuzan.Shaikh
-"""
-
-print('Data summary codes')
--- woe_binning/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Feb 26 14:32:12 2018
-
-@author: Mohammadsuzan.Shaikh
-"""
-
-print('WOE binning codes')
